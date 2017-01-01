34405bc0b4ea4ed688adf3af09172653210d5e27
Hui Jun <hjyam@umich.edu>
2016-12-31 22:24:23 -0800

    Compplete Synth component

--- frontend/components/synth/synth.jsx
+++ frontend/components/synth/synth.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NOTE_NAMES, TONES } from '../../util/tones';
+import Note from '../../util/note';
 
 class Synth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.notes = NOTE_NAMES.map(note => new Note(TONES[note]));
+  }
+
   render() {
     return (
       <div>
-        Synth
+        <ul>
+          {this.notes.map((note, idx) => (
+            <li key={idx}>{note.stop()}</li>
+          ))}
+        </ul>
       </div>
     );
   }
