eb8dd1bc0f179648dd97782a9e4eb7d805fd3b48
Hui Jun <hjyam@umich.edu>
2017-01-01 08:41:22 -0800

    Complete Synth

--- frontend/components/synth/synth.jsx
+++ frontend/components/synth/synth.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NOTE_NAMES, TONES } from '../../util/tones';
 import Note from '../../util/note';
+import $ from 'jquery';
+import NoteKey from './note_key';
 
 class Synth extends React.Component {
   constructor(props) {
@@ -8,14 +10,42 @@ class Synth extends React.Component {
     this.notes = NOTE_NAMES.map(note => new Note(TONES[note]));
   }
 
+  componentDidMount() {
+    $(document).on('keydown', e => this.onKeyDown(e));
+    $(document).on('keyup', e => this.onKeyUp(e));
+  }
+
+  onKeyDown(e) {
+    this.props.keyPressed(e.key);
+  }
+
+  onKeyUp(e) {
+    this.props.keyReleased(e.key);
+  }
+
+  playNotes() {
+    NOTE_NAMES.forEach((note, idx) => {
+      if (this.props.notes.indexOf(note) !== -1) {
+        this.notes[idx].start();
+      } else {
+        this.notes[idx].stop();
+      }
+    });
+  }
+
   render() {
+    this.playNotes();
+    const noteKey = NOTE_NAMES.map((note, idx) => (
+      <NoteKey key={idx} note={note} pressed={this.props.notes.includes(note)} />
+    ));
     return (
       <div>
-        <ul>
-          {this.notes.map((note, idx) => (
-            <li key={idx}>{note.stop()}</li>
-          ))}
-        </ul>
+        <div>
+          Redux Synthesizer
+        </div>
+        <div>
+          {noteKey}
+        </div>
       </div>
     );
   }
