857b1a8a414ea756c9d741c6c7e7aaad6ec3a71f
Hui Jun <hjyam@umich.edu>
2016-12-31 21:37:09 -0800

    Add root.jsx and app.jsx

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Note from './util/note';
 import configureStore from './store/store';
-
-// const App = () => {
-//
-// };
-
-// window.Note = Note;
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   const root = document.getElementById("root");
-  ReactDOM.render(<h1> Synthesizer</h1>, root);
-  window.store = store;
+  ReactDOM.render(<Root store={store} />, root);
 });
